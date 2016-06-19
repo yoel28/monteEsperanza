4fbfdc8727ee36c041c92f4cbac0d7c819ffa181
yo3l28 <yo3l.m18@gmail.com>
2016-06-18 21:00:22 -0400

    tipoEmpresa extiende de restcontroller

--- app/tipoEmpresa/tipoEmpresa.ts
+++ app/tipoEmpresa/tipoEmpresa.ts
@@ -3,64 +3,41 @@ import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
 import  {FormBuilder, Validators, Control, ControlGroup,} from '@angular/common';
 import {HttpUtils} from "../common/http-utils";
+import {RestController} from "../common/restController";
 
-//--------------------------LOGIN-------------------------------
 @Component({
     selector: 'tipoEmpresa',
     templateUrl: 'app/tipoEmpresa/tipoEmpresa.html',
     styleUrls: ['app/tipoEmpresa/tipoEmpresa.css']
 })
-export class TipoEmpresa {
-    tipoEmpresa:any=[];
-    httputils:HttpUtils;
-    endpoint:string;
+export class TipoEmpresa extends RestController{
 
+    title: Control;
+    icon: Control;
 
-    constructor(public router: Router,public http: Http, _formBuilder: FormBuilder) {
+    constructor(public router: Router,public http: Http,public _formBuilder: FormBuilder) {
+        super(http);
+        this.validTokens();
+        this.setEndpoint('/type/companies/');
+        this.initForm();
+        this.loadData();
+    }
+    initForm(){
+
+        this.title = new Control("", Validators.compose([Validators.required]));
+        this.icon = new Control("", Validators.compose([Validators.required]));
+
+        this.form = this._formBuilder.group({
+            title: this.title,
+            icon: this.icon,
+        });
+
+    }
+    validTokens(){
         if(!localStorage.getItem('bearer'))
         {
             let link = ['AccountLogin', {}];
             this.router.navigate(link);
         }
-        this.endpoint="/type/companies/";
-        this.httputils = new HttpUtils(http);
-        this.getTipoEmpresas();
-
-        this.title = new Control("", Validators.compose([Validators.required]));
-        this.icon = new Control("", Validators.compose([Validators.required]));
-        this.form = _formBuilder.group({
-            title: this.title,
-            icon: this.icon,
-        });
-    }
-    error=function(err){
-        console.log(err);
-    }
-
-    getTipoEmpresas(){
-        event.preventDefault();
-        this.httputils.onLoadList(this.endpoint,this.tipoEmpresa,this.error);
-    }
-    onUpdate(event,data){
-        //event.preventDefault();
-        if(data[event.target.accessKey]!=event.target.innerHTML){
-            data[event.target.accessKey] = event.target.innerHTML;
-            let body = JSON.stringify(data);
-            this.httputils.onUpdate(this.endpoint+data.id,body,data,this.error);
-        }
-    }
-    onDelete(event,id){
-        event.preventDefault();
-        this.httputils.onDelete(this.endpoint+id, id, this.tipoEmpresa, this.error);
-    }
-
-    form: ControlGroup;
-    title: Control;
-    icon: Control;
-
-    onSave(event: Event) {
-        event.preventDefault();
-        let body = JSON.stringify(this.form.value);
-        this.httputils.onSave(this.endpoint,body,this.tipoEmpresa,this.error);
     }
 }
