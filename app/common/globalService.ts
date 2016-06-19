c625485c9755651f9c4104bccdb53936766f7f78
yo3l28 <yo3l.m18@gmail.com>
2016-06-18 22:53:44 -0400

    Servicio Global

---
+++ app/common/globalService.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class globalService {
+    version:string = "1.0.0";
+    user:any={};
+}
--- app/main.ts
+++ app/main.ts
@@ -3,10 +3,11 @@ import { provide }    from '@angular/core';
 import { Http, HTTP_PROVIDERS } from '@angular/http';
 import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-
+import { globalService }   from './common/globalService';
 import { AppComponent }   from './app.component';
 
 bootstrap(AppComponent, [
+    globalService,
     HTTP_PROVIDERS,
     { provide: LocationStrategy, useClass: HashLocationStrategy },
     provide(AuthHttp, {
