cbda97f3cdc14da1a6fdb3392bb86a3e0c6ffc6d
yo3l28 <yo3l.m18@gmail.com>
2016-06-18 20:54:20 -0400

    tagRfid extiende de restcontroller

--- app/tagRfid/tagRfid.ts
+++ app/tagRfid/tagRfid.ts
@@ -1,67 +1,42 @@
 import { Component } from '@angular/core';
 import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
-import  {FormBuilder, Validators, Control, ControlGroup,} from '@angular/common';
-import {HttpUtils} from "../common/http-utils";
+import  {FormBuilder, Validators, Control} from '@angular/common';
+import {RestController} from "../common/restController";
 
 @Component({
     selector: 'tagRfid',
     templateUrl: 'app/tagRfid/tagRfid.html',
     styleUrls: ['app/tagRfid/tagRfid.css']
 })
-export class TagRfid {
-    dataList:any=[];
-    httputils:HttpUtils;
-    endpoint:string;
+export class TagRfid extends RestController{
 
+    vehicle: Control;
+    number: Control;
 
-    constructor(public router: Router,public http: Http, _formBuilder: FormBuilder) {
+    constructor(public router: Router,public http: Http,public _formBuilder: FormBuilder) {
+        super(http);
+        this.validTokens();
+        this.setEndpoint('/rfids/');
+        this.initForm();
+        this.loadData();
+    }
+    initForm(){
+
+        this.vehicle = new Control("", Validators.compose([Validators.required]));
+        this.number = new Control("", Validators.compose([Validators.required]));
+
+        this.form = this._formBuilder.group({
+            vehicle: this.vehicle,
+            number: this.number,
+        });
+        
+    }
+    validTokens(){
         if(!localStorage.getItem('bearer'))
         {
             let link = ['AccountLogin', {}];
             this.router.navigate(link);
         }
-        this.endpoint="/rfids/";
-        this.httputils = new HttpUtils(http);
-        this.loadData();
-
-        this.vehicle = new Control("", Validators.compose([Validators.required]));
-        this.number = new Control("", Validators.compose([Validators.required]));
-
-
-        this.form = _formBuilder.group({
-            vehicle: this.vehicle,
-            number: this.number,
-        });
-    }
-    error=function(err){
-        console.log(err);
-    }
-
-    loadData(){
-        event.preventDefault();
-        this.httputils.onLoadList(this.endpoint,this.dataList,this.error);
-    }
-    onUpdate(event,data){
-        //event.preventDefault();
-        if(data[event.target.accessKey]!=event.target.innerHTML){
-            data[event.target.accessKey] = event.target.innerHTML;
-            let body = JSON.stringify(data);
-            this.httputils.onUpdate(this.endpoint+data.id,body,this.dataList,this.error);
-        }
-    }
-    onDelete(event,id){
-        event.preventDefault();
-        this.httputils.onDelete(this.endpoint+id, id, this.dataList, this.error);
-    }
-
-    form: ControlGroup;
-    vehicle: Control;
-    number: Control;
-
-    onSave(event: Event) {
-        event.preventDefault();
-        let body = JSON.stringify(this.form.value);
-        this.httputils.onSave(this.endpoint,body,this.dataList,this.error);
     }
 }
