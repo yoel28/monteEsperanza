c84ac95b38c543f78ea02cec5f959c110076437b
yo3l28 <yo3l.m18@gmail.com>
2016-06-19 20:37:36 -0400

    tipoVehiculo con restcontroller modal agregado por directivas

--- app/tipoVehiculo/tipoVehiculo.ts
+++ app/tipoVehiculo/tipoVehiculo.ts
@@ -1,40 +1,22 @@
 import { Component } from '@angular/core';
 import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
-import  {FormBuilder, Validators, Control} from '@angular/common';
 import {RestController} from "../common/restController";
+import {TipoVehiculoSave} from "./methods";
 
-//--------------------------LOGIN-------------------------------
 @Component({
     selector: 'tipoVehiculo',
-    templateUrl: 'app/tipoVehiculo/tipoVehiculo.html',
-    styleUrls: ['app/tipoVehiculo/tipoVehiculo.css']
+    templateUrl: 'app/tipoVehiculo/index.html',
+    styleUrls: ['app/tipoVehiculo/style.css'],
+    directives: [TipoVehiculoSave],
 })
 export class TipoVehiculo extends RestController{
-
-    title: Control;
-    detail: Control;
-    icon: Control;
-
-    constructor(public router: Router,public http: Http,public _formBuilder: FormBuilder) {
+    
+    constructor(public router: Router,public http: Http) {
         super(http);
         this.validTokens();
         this.setEndpoint('/type/vehicles/');
-        this.initForm();
         this.loadData();
-    }
-    initForm(){
-
-        this.title = new Control("", Validators.compose([Validators.required]));
-        this.detail = new Control("", Validators.compose([Validators.required]));
-        this.icon = new Control("", Validators.compose([Validators.required]));
-
-        this.form = this._formBuilder.group({
-            title: this.title,
-            icon: this.icon,
-            detail: this.detail,
-        });
-
     }
     validTokens(){
         if(!localStorage.getItem('bearer'))
