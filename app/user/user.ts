9b2caf66c1db2e905bce3ccf2fa08cf472658169
yo3l <yo3l.m18@gmail.com>
2016-06-20 17:57:16 -0400

    user con directivas y modelos

--- app/user/user.ts
+++ app/user/user.ts
@@ -4,15 +4,18 @@ import { Http} from '@angular/http';
 import {RestController} from "../common/restController";
 import {globalService} from "../common/globalService";
 import {UserSave} from "./methods";
+import {Search} from "../utils/search/search";
+import {EmpresaSave} from "../empresa/methods";
 
 @Component({
     selector: 'user',
     templateUrl: 'app/user/index.html',
     styleUrls: ['app/user/style.css'],
-    directives: [UserSave],
-
+    directives: [UserSave,Search,EmpresaSave],
 })
 export class User extends RestController{
+    
+    public userSelect:string;
 
     constructor(public router: Router,public http: Http,public myglobal:globalService) {
         super(http);
@@ -32,5 +35,20 @@ export class User extends RestController{
         let link = ['TaquillaSearh', {search:companyRuc}];
         this.router.navigate(link);
     }
+
+
+    public searchEmpresa={
+        title:"Empresa",
+        idModal:"searchEmpresa",
+        endpointForm:"consultas/search.json",
+        placeholderForm:"Ingrese el RUC de la empresa",
+        labelForm:{name:"Nombre: ",detail:"Detalle: "},
+    }
+
+    assignCompany(data){
+        let index = this.dataList.list.findIndex(obj => obj.id == this.userSelect);
+        this.onPatch('company',this.dataList.list[index],data.id);
+    }
+   
 
 }
