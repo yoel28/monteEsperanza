b2b173d84a50697ddd077c299434ab64416116ac
yo3l <yo3l.m18@gmail.com>
2016-06-20 17:58:28 -0400

    search por directivas y modelo

---
+++ app/utils/search/search.ts
@@ -0,0 +1,30 @@
+import { Component,EventEmitter } from '@angular/core';
+import { Http} from '@angular/http';
+import {RestController} from "../../common/restController";
+
+@Component({
+    selector: 'search',
+    templateUrl: 'app/utils/search/index.html',
+    styleUrls: ['app/utils/search/style.css'],
+    inputs:['params'],
+    outputs:['result'],
+})
+export class Search extends RestController{
+
+    public params:any={};
+    public result:any;
+
+    constructor(public http:Http) {
+        super(http);
+        this.setEndpoint(this.params.endpointForm);
+        this.result = new EventEmitter();
+    }
+    getSearch(search){
+        //TODO: this.httputils.onLoadList(this.endpointForm+search,this.dataList,this.error);
+        this.httputils.onLoadList(this.params.endpointForm,this.dataList,this.error,true);
+    }
+    getData(data){
+        this.result.emit(data);
+    }
+}
+
