38291dadfe8177a9e6258d6a503b7969ac4f5d5c
yo3l28 <yo3l.m18@gmail.com>
2016-06-19 22:24:51 -0400

    angular modo producion

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,12 @@
 import { bootstrap }      from '@angular/platform-browser-dynamic';
-import { provide }    from '@angular/core';
+import { provide,enableProdMode }    from '@angular/core';
 import { Http, HTTP_PROVIDERS } from '@angular/http';
 import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { globalService }   from './common/globalService';
 import { AppComponent }   from './app.component';
 
+enableProdMode();
 bootstrap(AppComponent, [
     globalService,
     HTTP_PROVIDERS,
