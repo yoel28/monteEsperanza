959ec55cb3a25a23b617124dbaca053af73e5193
yo3l <yo3l.m18@gmail.com>
2016-06-21 15:17:46 -0400

    app con objeto global

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,6 @@ import { Router,RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angula
 import { contentHeaders } from './common/headers';
 
 import { AccountLogin }         from './account/account';
-import { AccountSignup }         from './account/account';
-
 import { Empresa }         from './empresa/empresa';
 import { TipoEmpresa }         from './tipoEmpresa/tipoEmpresa';
 import { TipoVehiculo }         from './tipoVehiculo/tipoVehiculo';
@@ -16,6 +14,7 @@ import { Parametro }         from './parametro/parametro';
 import { Regla }         from './regla/regla';
 import { TipoRecarga }         from './tipoRecarga/tipoRecarga';
 import { TagRfid }         from './tagRfid/tagRfid';
+import {globalService} from "./common/globalService";
 
 @Component({
   selector: 'my-app',
@@ -28,14 +27,12 @@ import { TagRfid }         from './tagRfid/tagRfid';
 })
 @RouteConfig([
   { path: '/account/login',  name: 'AccountLogin',  component: AccountLogin, useAsDefault: true },
-    
-  { path: '/account/signup',  name: 'AccountSignup',  component: AccountSignup},
-    
-  { path: '/user',   name: 'User', component: User },
+
+  { path: '/users',   name: 'User', component: User },
   { path: '/dashboard',   name: 'Dashboard', component: Dashboard },
   { path: '/taquilla',   name: 'Taquilla', component: Taquilla },
   { path: '/taquilla/:search',   name: 'TaquillaSearh', component: Taquilla },
-  { path: '/empresa',   name: 'Empresa', component: Empresa },
+  { path: '/empresas',   name: 'Empresa', component: Empresa },
   { path: '/operacion',   name: 'Operacion', component: Operacion },
   { path: '/parametro',   name: 'Parametro', component: Parametro },
   { path: '/regla',   name: 'Regla', component: Regla },
@@ -47,10 +44,10 @@ import { TagRfid }         from './tagRfid/tagRfid';
 ])
 export class AppComponent {
 
-  constructor(private router: Router) {
+  constructor(private router: Router,public myglobal:globalService) {
     //TODO:Cambiar URL a PRODUCCION
-    localStorage.setItem('urlAPI','http://ec2-54-197-11-239.compute-1.amazonaws.com:8080/api');
-    //localStorage.setItem('urlAPI','http://192.168.0.91:8080/api');
+    //localStorage.setItem('urlAPI','http://ec2-54-197-11-239.compute-1.amazonaws.com:8080/api');
+    localStorage.setItem('urlAPI','http://192.168.0.91:8080/api');
   }
   logout() {
     localStorage.removeItem('bearer');
