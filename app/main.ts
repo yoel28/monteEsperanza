028afe190f803cd99f6da54ac9e9342ba9ed5b76
yo3l <yo3l.m18@gmail.com>
2016-06-24 17:45:56 -0400

    toast global

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,13 @@ import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { globalService }   from './common/globalService';
 import { AppComponent }   from './app.component';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
 
 enableProdMode();
 bootstrap(AppComponent, [
     globalService,
+    ToastsManager,
     HTTP_PROVIDERS,
     { provide: LocationStrategy, useClass: HashLocationStrategy },
     provide(AuthHttp, {
