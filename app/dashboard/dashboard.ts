517def524598268d888891e7767c144d03425df6
yo3l <yo3l.m18@gmail.com>
2016-06-24 17:41:26 -0400

    dashboard con imagenes del usuario toast y timeline con ruc

--- app/dashboard/dashboard.ts
+++ app/dashboard/dashboard.ts
@@ -6,6 +6,7 @@ import { Ng2Highcharts } from 'ng2-highcharts/ng2-highcharts';
 import {RecargaTimeLine} from "../recarga/methods";
 import match = require("core-js/fn/symbol/match");
 import {RestController} from "../common/restController";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
 
 
 @Component({
@@ -22,8 +23,8 @@ export class Dashboard extends RestController{
     public paramsTimeLine={
         'offset':0,
         'max':3,
+        'ruc':''
     };
-    
     dataArea={
         chart: {
           type: 'area',
@@ -43,8 +44,8 @@ export class Dashboard extends RestController{
         title : { text : 'Uso del vertedero' },
     };
 
-    constructor(public router: Router,http: Http) {
-        super(http);
+    constructor(public router: Router,http: Http,public toastr: ToastsManager) {
+        super(http,toastr);
         this.validTokens();
         this.getPlot1();
         this.getPlot2();
