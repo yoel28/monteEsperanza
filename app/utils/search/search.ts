6640f58e84d52034bd9a3a787345c0e5cecfb6b0
yo3l28 <yo3l.m18@gmail.com>
2016-06-24 22:49:58 -0400

    search con parametro where

--- app/utils/search/search.ts
+++ app/utils/search/search.ts
@@ -18,6 +18,7 @@ export class Search extends RestController{
     //     endpointForm:"/search/vehicles/",
     //     placeholderForm:"Ingrese la placa",
     //     labelForm:{name:"Nombre: ",detail:": "},
+    //     where:&where[['op':'eq','field':'vehicle','value':'IsNull']]
     // }
 
     public params:any={};
@@ -29,9 +30,16 @@ export class Search extends RestController{
         this.result = new EventEmitter();
     }
     getSearch(search){
-        this.endpoint=this.params.endpointForm+search
+        this.endpoint=this.params.endpointForm+search;
         this.loadData();
     }
+    loadData(offset=0){
+        this.offset=offset;
+        if(this.params.where)
+            this.httputils.onLoadList(this.endpoint+"?max="+this.max+"&offset="+this.offset+this.params.where,this.dataList,this.max,this.error);
+        else
+            this.httputils.onLoadList(this.endpoint+"?max="+this.max+"&offset="+this.offset,this.dataList,this.max,this.error);
+    };
     getData(data){
         this.result.emit(data);
     }
