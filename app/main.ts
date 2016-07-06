62e8250505eb72a34d14a091e55e0ca6ec17d894
yo3l28 <yo3l.m18@gmail.com>
2016-07-05 21:54:06 -0400

    7sg duracion del toast

--- app/main.ts
+++ app/main.ts
@@ -5,13 +5,18 @@ import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from '@angular/common';
 import { globalService }   from './common/globalService';
 import { AppComponent }   from './app.component';
-import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+import { ToastsManager,ToastOptions } from 'ng2-toastr/ng2-toastr';
 import 'semantic';
+
+let options = {
+    toastLife: 7000,
+};
 
 enableProdMode();
 bootstrap(AppComponent, [
     globalService,
     ToastsManager,
+    provide(ToastOptions, { useValue: new ToastOptions(options)}),
     HTTP_PROVIDERS,
     provide(LocationStrategy, { useClass: HashLocationStrategy }),
     provide(APP_BASE_HREF, { useValue: '/' }),
