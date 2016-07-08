aa65f673f5c548d3053b9914d377b297e5a31296
yo3l <yo3l.m18@gmail.com>
2016-07-08 17:35:10 -0400

    agregada restriccion por permisos creadas las reglas de tipovehiculo

--- app/tipoVehiculo/tipoVehiculo.ts
+++ app/tipoVehiculo/tipoVehiculo.ts
@@ -3,6 +3,8 @@ import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
 import {RestController} from "../common/restController";
 import {TipoVehiculoSave} from "./methods";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
 
 @Component({
     selector: 'tipoVehiculo',
@@ -11,11 +13,27 @@ import {TipoVehiculoSave} from "./methods";
     directives: [TipoVehiculoSave],
 })
 export class TipoVehiculo extends RestController{
+
+    public rules={
+        'id': {'type':'number','disabled':true,'display':false,'title':'id','placeholder':'Identificador','search':true},
+        'title':{'type':'text','display':null,'title':'Titulo','placeholder':'Titulo','search':true},
+        'detail':{'type':'text','display':null,'title':'nombre','placeholder':'Nombre de usuario','search':true},
+        'icon':{'type':'select','display':null,'title':'Icono','mode':'inline',
+            'source': [
+                {'value': 'fa fa-truck', 'text': 'String'},
+                {'value': 'Long', 'text': 'Long'},
+                {'value': 'Double', 'text': 'Double'},
+                {'value': 'Date', 'text': 'Date'},
+            ]
+        },
+    }
     
-    constructor(public router: Router,public http: Http) {
-        super(http);
-        this.validTokens();
+    constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService) {
+        super(http,toastr);
         this.setEndpoint('/type/vehicles/');
+    }
+    ngOnInit(){
+        this.validTokens();
         this.loadData();
     }
     validTokens(){
