a711a8b339f4d6dc13ed392a756f121fc0fe830c
yo3l28 <yo3l.m18@gmail.com>
2016-07-09 20:02:47 -0400

    Vista (xeditable,permisos,confirmacion al borrar) controlador (rules y directiva xeditable)

--- app/tipoVehiculo/tipoVehiculo.ts
+++ app/tipoVehiculo/tipoVehiculo.ts
@@ -5,25 +5,26 @@ import {RestController} from "../common/restController";
 import {TipoVehiculoSave} from "./methods";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
 import {globalService} from "../common/globalService";
+import {Xeditable} from "../common/xeditable";
 
 @Component({
     selector: 'tipoVehiculo',
     templateUrl: 'app/tipoVehiculo/index.html',
     styleUrls: ['app/tipoVehiculo/style.css'],
-    directives: [TipoVehiculoSave],
+    directives: [TipoVehiculoSave,Xeditable],
 })
 export class TipoVehiculo extends RestController{
-
+    
     public rules={
         'id': {'type':'number','disabled':true,'display':false,'title':'id','placeholder':'Identificador','search':true},
         'title':{'type':'text','display':null,'title':'Titulo','placeholder':'Titulo','search':true},
         'detail':{'type':'text','display':null,'title':'nombre','placeholder':'Nombre de usuario','search':true},
         'icon':{'type':'select','display':null,'title':'Icono','mode':'inline',
             'source': [
-                {'value': 'fa fa-truck', 'text': 'String'},
-                {'value': 'Long', 'text': 'Long'},
-                {'value': 'Double', 'text': 'Double'},
-                {'value': 'Date', 'text': 'Date'},
+                {'value': 'fa fa-truck', 'text':'truck'},
+                {'value': 'fa fa-car', 'text': 'car'},
+                {'value': 'fa fa-bus', 'text': 'bus'},
+                {'value': 'fa fa-taxi', 'text': 'taxi'},
             ]
         },
     }
