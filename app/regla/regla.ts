82ab53cf96d3f861ad14762b28c985c3610459b8
yo3l28 <yo3l.m18@gmail.com>
2016-07-09 22:34:42 -0400

    reglas del modelo agregado ngOnInit y cambio de push a unshift

--- app/regla/regla.ts
+++ app/regla/regla.ts
@@ -3,19 +3,30 @@ import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
 import {RestController} from "../common/restController";
 import {ReglaSave} from "./methods";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import {Xeditable} from "../common/xeditable";
 
 @Component({
     selector: 'regla',
     templateUrl: 'app/regla/index.html',
     styleUrls: ['app/regla/style.css'],
-    directives : [ReglaSave]
+    directives : [ReglaSave,Xeditable]
 })
 export class Regla extends RestController{
 
-    constructor(public router: Router,public http: Http) {
-        super(http);
-        this.validTokens();
+    public rules={
+        'id': {'type':'number','disabled':true,'display':false,'title':'id','placeholder':'Identificador','search':true},
+        'rule':{'type':'text','display':null,'title':'Regla','placeholder':'regla','search':true},
+        'name':{'type':'text','display':null,'title':'Nombre','placeholder':'Nombre','search':true},
+    };
+
+    constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService) {
+        super(http,toastr);
         this.setEndpoint('/rules/');
+    }
+    ngOnInit(){
+        this.validTokens();
         this.loadData();
     }
     validTokens(){
@@ -26,6 +37,6 @@ export class Regla extends RestController{
         }
     }
     assignRule(data){
-        this.dataList.list.push(data);
+        this.dataList.list.unshift(data);
     }
 }
