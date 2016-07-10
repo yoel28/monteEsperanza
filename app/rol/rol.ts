6a44b70e58fb83145ff854a163ce19b24451cfd6
yo3l28 <yo3l.m18@gmail.com>
2016-07-09 22:09:39 -0400

    reglas del modelo

--- app/rol/rol.ts
+++ app/rol/rol.ts
@@ -4,19 +4,27 @@ import { Http } from '@angular/http';
 import {RestController} from "../common/restController";
 import {RolSave} from "./methods";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import {Xeditable} from "../common/xeditable";
 
 @Component({
     selector: 'rol',
     templateUrl: 'app/rol/index.html',
     styleUrls: ['app/rol/style.css'],
-    directives:[RolSave]
+    directives:[RolSave,Xeditable]
 })
 export class Rol extends RestController{
 
-    constructor(public router: Router,public http: Http,public toastr: ToastsManager) {
+    public rules={
+        'id': {'type':'number','disabled':true,'display':false,'title':'id','placeholder':'Identificador','search':true},
+        'authority':{'type':'text','display':null,'title':'Titulo','placeholder':'authority','search':true},
+    }
+    constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService) {
         super(http,toastr);
-        this.validTokens();
         this.setEndpoint('/roles/');
+    }
+    ngOnInit(){
+        this.validTokens();
         this.loadData();
     }
     validTokens(){
