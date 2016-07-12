8a28bec2f94cea785985f53da43ce18f20f36e3f
yo3l28 <yo3l.m18@gmail.com>
2016-07-12 10:57:27 -0400

    agregado globalservice y parametros del filter

--- app/parametro/parametro.ts
+++ app/parametro/parametro.ts
@@ -5,24 +5,26 @@ import {RestController} from "../common/restController";
 import {ParametroSave} from "./methods";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
 import {Xeditable} from "../common/xeditable";
+import {Filter} from "../utils/filter/filter";
+import {globalService} from "../common/globalService";
 
 @Component({
     selector: 'parametro',
     templateUrl: 'app/parametro/index.html',
     styleUrls: ['app/parametro/style.css'],
-    directives:[ParametroSave,Xeditable]
+    directives:[ParametroSave,Xeditable,Filter]
 })
 export class Parametro extends RestController{
 
-    constructor(public router: Router,public http: Http,public toastr: ToastsManager) {
+    constructor(public router: Router,public http: Http,public toastr: ToastsManager,public myglobal:globalService) {
         super(http,toastr);
         this.setEndpoint('/params/');
     }
     public rules={
-        'id': {'type':'text','disabled':true,'display':false,'title':'' },
-        'key':{'type':'text','display':null,'title':'Key','mode':'inline'},
-        'value':{'type':'text','display':null,'title':'Valor','mode':'inline' },
-        'type':{'type':'select','display':null,'title':'Tipo','mode':'inline',
+        'id': {'type':'number','disabled':true,'display':false,'title':'','placeholder': 'Identificador', 'search': true},
+        'key':{'type':'text','display':null,'title':'Key','mode':'inline','placeholder': 'Clave', 'search': true},
+        'value':{'type':'text','display':null,'title':'Valor','mode':'inline','placeholder': 'Valor', 'search': true},
+        'type':{'type':'select','display':null,'title':'Tipo','mode':'inline','placeholder': 'Tipo', 'search': true,
             'source': [
                 {'value': 'String', 'text': 'String'},
                 {'value': 'Long', 'text': 'Long'},
@@ -33,19 +35,21 @@ export class Parametro extends RestController{
     };
     ngOnInit(){
         this.max = 10;
-        this.validTokens();
         this.loadData();
-    }
-    validTokens(){
-        if(!localStorage.getItem('bearer'))
-        {
-            let link = ['AccountLogin', {}];
-            this.router.navigate(link);
-        }
     }
     assignParametro(data){
         this.dataList.list.unshift(data);
         this.dataList.list.pop();
+    }
+    //Cargar Where del filter
+    public paramsFilter:any = {
+        title: "Filtrar Parametros",
+        idModal: "modalFilter",
+        endpointForm: "",
+    };
+    loadWhere(where) {
+        this.where = where;
+        this.loadData();
     }
 
 }
