4f4e5f8ac9a77e3fe69676ae960762625a0cb950
yo3l <yo3l.m18@gmail.com>
2016-07-13 17:01:18 -0400

    cargar vehiculos con permisos

--- app/taquilla/taquilla.ts
+++ app/taquilla/taquilla.ts
@@ -6,6 +6,7 @@ import {RecargaSave} from "../recarga/methods";
 import {RestController} from "../common/restController";
 import {Fecha} from "../utils/pipe";
 import {globalService} from "../common/globalService";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
 
 
 @Component({
@@ -20,36 +21,43 @@ export class Taquilla extends RestController{
     dataTruck:any = {};
     search;
 
-    constructor(public params:RouteParams,public router: Router,http: Http,_formBuilder: FormBuilder,public myglobal:globalService) {
-        super(http);
+    constructor(public params:RouteParams,public router: Router,http: Http,_formBuilder: FormBuilder,public toastr: ToastsManager,public myglobal:globalService) {
+        super(http,toastr);
     }
     ngOnInit(){
         if(this.params.get('search'))
         {
-            this.getVehicles(this.params.get('search'));
+            if(this.myglobal.existsPermission('69'))
+                this.getVehicles(this.params.get('search'));
             this.search = this.params.get('search');
         }
     }
     getVehicle(truckId:string){
-        let successCallback= response => {
-            Object.assign(this.dataTruck, response.json());
-            this.dataTruck['recharges']=[];
-            this.loadData();
+        if(this.myglobal.existsPermission('69')){
+            let successCallback= response => {
+                Object.assign(this.dataTruck, response.json());
+                this.dataTruck['recharges']=[];
+                this.loadData();
+            }
+            this.httputils.doGet("/vehicles/"+truckId,successCallback,this.error)
+            this.dataVehicles = {};
         }
-        this.httputils.doGet("/vehicles/"+truckId,successCallback,this.error)
-        this.dataVehicles = {};
     }
     loadData(offset=0){
-        this.offset=offset;
-        this.endpoint="/search/recharges";
-        this.httputils.onLoadList(this.endpoint+"?where=[['op':'eq','field':'vehicle.id','value':"+this.dataTruck.id+"l]]"+"&max="+this.max+"&offset="+this.offset,this.dataList,this.max,this.error);
+        if(this.myglobal.existsPermission('109')) {
+            this.offset = offset;
+            this.endpoint = "/search/recharges";
+            let where = encodeURI("[['op':'eq','field':'vehicle.id','value':" + this.dataTruck.id + "l]]")
+            this.httputils.onLoadList(this.endpoint + "?where=" + where + "&max=" + this.max + "&offset=" + this.offset, this.dataList, this.max, this.error);
+        }
     };
 
     getVehicles(camion:string,offset=0){
-        this.offset=offset;
-        this.dataTruck = {};
-        this.httputils.onLoadList("/search/vehicles/"+camion+"?max="+this.max+"&offset="+this.offset, this.dataVehicles,this.max,this.error);
-
+        if(this.myglobal.existsPermission('69')) {
+            this.offset = offset;
+            this.dataTruck = {};
+            this.httputils.onLoadList("/search/vehicles/" + camion + "?max=" + this.max + "&offset=" + this.offset, this.dataVehicles, this.max, this.error);
+        }
     }
     assignRecarga(data){
         this.dataTruck.balance+=data.quantity;
