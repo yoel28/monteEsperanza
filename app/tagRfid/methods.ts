9f14a32f23aa59bf44eba4ec4e0f0082096c0074
yo3l <yo3l.m18@gmail.com>
2016-07-13 15:47:07 -0400

    mensaje de toast

--- app/tagRfid/methods.ts
+++ app/tagRfid/methods.ts
@@ -35,6 +35,7 @@ export class TagSave extends RestController{
     submitForm(){
         let successCallback= response => {
             this.save.emit(response.json());
+            this.toastr.success('Guardado con éxito','Notificación')
         };
         let body = JSON.stringify(this.form.value);
         this.httputils.doPost(this.endpoint,body,successCallback,this.error);
