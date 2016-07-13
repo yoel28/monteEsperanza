cf1b848b475eefe2e39b0c4fe711ebddc5e81d80
yo3l <yo3l.m18@gmail.com>
2016-07-13 17:28:03 -0400

    dataselect rules parametros del filter

--- app/tipoEmpresa/tipoEmpresa.ts
+++ app/tipoEmpresa/tipoEmpresa.ts
@@ -6,18 +6,22 @@ import {TipoEmpresaSave} from "./methods";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
 import {globalService} from "../common/globalService";
 import {Xeditable} from "../common/xeditable";
+import {Filter} from "../utils/filter/filter";
 
 @Component({
     selector: 'tipoEmpresa',
     templateUrl: 'app/tipoEmpresa/index.html',
     styleUrls: ['app/tipoEmpresa/style.css'],
-    directives: [TipoEmpresaSave,Xeditable],
+    directives: [TipoEmpresaSave,Xeditable,Filter],
 })
 export class TipoEmpresa extends RestController{
+
+    public dataSelect:any={};
+
     public rules={
         'id': {'type':'number','disabled':true,'display':false,'title':'id','placeholder':'Identificador','search':true},
         'title':{'type':'text','display':null,'title':'Titulo','placeholder':'Titulo','search':true},
-        'detail':{'type':'text','display':null,'title':'nombre','placeholder':'Nombre de usuario','search':true},
+        'detail':{'type':'text','display':null,'title':'nombre','placeholder':'Detalle','search':true},
         'icon':{'type':'select','display':null,'title':'Icono','mode':'inline',
             'source': [
                 {'value': 'fa fa-building', 'text':'building'},
@@ -31,17 +35,25 @@ export class TipoEmpresa extends RestController{
         this.setEndpoint('/type/companies/');
     }
     ngOnInit(){
-        this.validTokens();
-        this.loadData();
-    }
-    validTokens(){
-        if(!localStorage.getItem('bearer'))
-        {
-            let link = ['AccountLogin', {}];
-            this.router.navigate(link);
+        if (this.myglobal.existsPermission('36')) {
+            this.loadData();
         }
     }
     assignTipoEmpresa(data){
         this.dataList.list.unshift(data);
+        this.dataList.list.pop();
+    }
+    //Cargar Where del filter
+    public paramsFilter:any = {
+        title: "Filtrar tipos de empresas",
+        idModal: "modalFilter",
+        endpointForm: "",
+    };
+
+    loadWhere(where) {
+        this.where = where;
+        if (this.myglobal.existsPermission('36')) {
+            this.loadData();
+        }
     }
 }
