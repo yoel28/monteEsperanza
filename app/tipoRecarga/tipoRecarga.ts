7a8b43a79c315a03de7853044a2c526bec74f6af
yo3l <yo3l.m18@gmail.com>
2016-07-13 18:00:45 -0400

    reglas  toast  globalservice  eliminado validar token  parametros del filter

--- app/tipoRecarga/tipoRecarga.ts
+++ app/tipoRecarga/tipoRecarga.ts
@@ -3,30 +3,71 @@ import { Router }           from '@angular/router-deprecated';
 import { Http } from '@angular/http';
 import {RestController} from "../common/restController";
 import {TipoRecargaSave} from "./methods";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import {Filter} from "../utils/filter/filter";
+import {Xeditable} from "../common/xeditable";
 
 @Component({
     selector: 'tipoRecarga',
     templateUrl: 'app/tipoRecarga/index.html',
     styleUrls: ['app/tipoRecarga/style.css'],
-    directives: [TipoRecargaSave],
+    directives: [TipoRecargaSave,Xeditable,Filter],
 
 })
 export class TipoRecarga extends RestController{
     
-    constructor(public router: Router,public http: Http) {
+    constructor(public router: Router,public http: Http, public toastr:ToastsManager, public myglobal:globalService) {
         super(http);
-        this.validTokens();
         this.setEndpoint('/type/recharges/');
-        this.loadData();
+
     }
-    validTokens(){
-        if(!localStorage.getItem('bearer'))
-        {
-            let link = ['AccountLogin', {}];
-            this.router.navigate(link);
-        }
+    public dataSelect:any={};
+    public rules = {
+        'id': {
+            'type': 'number',
+            'disabled': true,
+            'display': false,
+            'title': 'id',
+            'placeholder': 'Identificador',
+            'search': true
+        },
+        'title': {
+            'type': 'text',
+            'display': null,
+            'title': 'Titulo',
+            'placeholder': 'Titulo',
+            'mode': 'inline',
+            'search': true
+        },
+        'detail': {
+            'type': 'text',
+            'display': null,
+            'title': 'Detalle',
+            'placeholder': 'Detalle',
+            'mode': 'inline',
+            'search': true
+        },
     }
+    ngOnInit() {
+        if(this.myglobal.existsPermission('116'))
+            this.loadData();
+    }
+
     assignTipoRecarga(data){
-        this.dataList.list.push(data);
+        this.dataList.list.unshift(data);
+        this.dataList.list.pop();
+    }
+    //Cargar Where del filter
+    public paramsFilter:any = {
+        title: "Filtrar tipos de recargas",
+        idModal: "modalFilter",
+        endpointForm: "",
+    };
+
+    loadWhere(where) {
+        this.where = where;
+        if(this.myglobal.existsPermission('116'))
+            this.loadData();
     }
 }
