d6a5d825c9ebecf7b426402cd4dbfe60111ffb8b
yo3l28 <yo3l.m18@gmail.com>
2016-07-13 20:47:38 -0400

    toast al guardar

--- app/tipoVehiculo/methods.ts
+++ app/tipoVehiculo/methods.ts
@@ -64,6 +64,7 @@ export class TipoVehiculoSave extends RestController{
     submitForm(){
         let successCallback= response => {
             this.save.emit(response.json());
+            this.toastr.success('Guardado con éxito','Notificación')
         };
         let body = JSON.stringify(this.form.value);
         this.httputils.doPost(this.endpoint,body,successCallback,this.error);
