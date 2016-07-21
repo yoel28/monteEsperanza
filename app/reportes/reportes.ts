aee4f8f66b4dd5cf97febb6038564e3fa38a98a8
yo3l <yo3l.m18@gmail.com>
2016-07-21 18:18:04 -0400

    reportes

---
+++ app/reportes/reportes.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { Router }           from '@angular/router-deprecated';
+import { Http } from '@angular/http';
+import {RestController} from "../common/restController";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import {Filter} from "../utils/filter/filter";
+import {FormBuilder, ControlGroup, Control, Validators} from "@angular/common";
+import moment from 'moment/moment';
+import {Datepicker} from "../common/xeditable";
+import {Fecha} from "../utils/pipe";
+
+
+@Component({
+    selector: 'reporte-grupos',
+    templateUrl: 'app/reportes/grupos.html',
+    pipes: [Fecha],
+    styleUrls: ['app/reportes/style.css'],
+    directives : [Filter,Datepicker]
+})
+export class ReporteGrupos extends RestController{
+
+    form: ControlGroup;
+    dateStart:Control;
+    dateEnd:Control;
+
+    constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService,public _formBuilder: FormBuilder) {
+        super(http,toastr);
+        this.setEndpoint('/reports/groups');
+    }
+    ngOnInit(){
+        this.initForm();
+    }
+    initForm(){
+        this.dateStart = new Control("", Validators.compose([Validators.required]));
+        this.dateEnd = new Control("");
+
+        this.form = this._formBuilder.group({
+            dateStart: this.dateStart,
+            dateEnd: this.dateEnd,
+        });
+    }
+    public formatDateFact = {
+        format: "dd/mm/yyyy",
+        startView: 2,
+        minViewMode: 0,
+        maxViewMode: 2,
+        language: "es",
+        forceParse: false,
+        autoclose: true,
+        todayBtn: "linked",
+        todayHighlight: true,
+    }
+    public params:any={};
+
+    loadFechaFac(data) {
+        if (data.key == "1")
+            this.dateStart.updateValue(data.date)
+        else
+            this.dateEnd.updateValue(data.date)
+    }
+
+    loadReporte(event){
+        event.preventDefault();
+        let final=this.dateEnd.value;
+        if (!this.dateEnd.value) {
+            final = (moment(this.dateStart.value).add(1, 'days'));
+        }
+
+        this.params = {
+            'dateStart': moment(this.dateStart.value.toString()).format('DD-MM-YYYY'),
+            'dateEnd': moment(final.toString()).format('DD-MM-YYYY'),
+        };
+        
+        let where ="[['op':'ge','field':'dateCreated','value':'"+this.params.dateStart+"','type':'date']," +
+            "['op':'lt','field':'dateCreated','value':'"+this.params.dateEnd+"','type':'date']]";
+        
+        this.where = "&where="+encodeURI(where);
+        this.max=100;
+        this.loadData();
+    }
+}
