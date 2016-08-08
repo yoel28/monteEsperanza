5f4cc5c82ddd2d44b1128fe9518a929252884b9e
yo3l <yo3l.m18@gmail.com>
2016-08-08 11:26:37 -0400

    collapseWhitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
         dist: {
             options: {
                 removeComments: true,
-                collapseWhitespace: false
+                collapseWhitespace: true
             },
             files: [{
                 expand: true,
