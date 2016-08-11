e155a10752ba87d7d31a09db94eb3ea22c82b2f2
yo3l <yo3l.m18@gmail.com>
2016-08-11 11:42:00 -0400

    descarga por rutas

---
+++ app/reportes/reporteDR.ts
@@ -0,0 +1,122 @@
+import {Component, OnInit} from '@angular/core';
+import { Router }           from '@angular/router-deprecated';
+import { Http } from '@angular/http';
+import {RestController} from "../common/restController";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import {Filter} from "../utils/filter/filter";
+import {FormBuilder} from "@angular/common";
+import {Datepicker} from "../common/xeditable";
+import {Fecha} from "../utils/pipe";
+import {Search} from "../utils/search/search";
+
+declare var jQuery:any;
+
+@Component({
+    selector: 'reporte-dr',
+    templateUrl: 'app/reportes/descargasRutas.html',
+    pipes: [Fecha],
+    styleUrls: ['app/reportes/style.css'],
+    directives : [Filter,Datepicker,Search]
+})
+export class ReporteDescargasRutas extends RestController implements OnInit{
+
+    constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService,public _formBuilder: FormBuilder) {
+        super(http,toastr);
+        this.setEndpoint('/report/weight/routes');
+    }
+    ngOnInit(){
+    }
+    public routeName:string="all";
+    setType(data){
+        this.routeName=data;
+        if(this.routeName!="-1")
+            this.loadMatriz();
+    }
+    public msgLabel:boolean=true;
+    cambiar(){
+        this.msgLabel=!this.msgLabel;
+    }
+    public formatDate1 = {
+        format: "mm/yyyy",
+        startView: 2,
+        minViewMode: 1,
+        maxViewMode: 2,
+        todayBtn: "linked",
+        language: "es",
+        forceParse: false,
+        autoclose: true,
+        todayHighlight: true,
+        return: 'YYYY/MM',
+    }
+    public formatDate2 = {
+        format: "yyyy",
+        startView: 2,
+        minViewMode: 2,
+        maxViewMode: 2,
+        todayBtn: "linked",
+        language: "es",
+        forceParse: false,
+        autoclose: true,
+        todayHighlight: true,
+        return: 'YYYY',
+    }
+    public dataConsult:any={'date':'2016'};
+
+    loadMatriz(data?) {
+        if(data){
+            this.dataConsult=data
+        }
+
+        let that = this;
+        let type="";
+        if(this.routeName!="all")
+            type="?type="+this.routeName;
+
+        let successCallback = response => {
+            Object.assign(that.dataList,response.json())
+        };
+        this.httputils.doGet('/reports/weight/routes/'+this.dataConsult.date+type,successCallback,this.error);
+    }
+    exportCSV(){
+        jQuery("#content").tableToCSV();
+    }
+
+    minMaxAvgSum(data){
+        let min=9999999999999999999;
+        let max=-999999999999999999;
+        let total=0.0;
+        let avg=0.0;
+        data.forEach(val=>{
+            if(val<min)
+                min=val;
+            if(val>max)
+                max=val;
+            total+=val;
+        })
+        avg=total/data.length;
+        return [min,max,avg.toFixed(3),total];
+    }
+
+    public searchRuta = {
+        title: "Ruta",
+        idModal: "searchModal",
+        endpointForm: "/search/routes/",
+        placeholderForm: "Ingrese la ruta",
+        labelForm: {name: "Nombre: ", detail: "Detalle: "},
+    }
+    assignRuta(data){
+        this.msgLabelAll=false;
+        this.setType(data.title);
+    }
+    public msgLabelAll:boolean=true;
+    cambiarAll(){
+        if(!this.msgLabelAll){
+            this.setType('all');
+        }
+        this.msgLabelAll=true;
+    }
+
+}
+
+
