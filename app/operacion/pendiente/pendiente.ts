0ade4a0b5a0d50797ff6e2716e21e4e1cdea0e5e
yoel <yo3l.m18@gmail.com>
2016-08-30 18:28:56 -0400

    nuevo controlador

---
+++ app/operacion/pendiente/pendiente.ts
@@ -0,0 +1,79 @@
+import {Component, OnInit,ViewChild} from '@angular/core';
+import {Router}           from '@angular/router-deprecated';
+import {Http} from '@angular/http';
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../../common/globalService";
+import {ModelBase} from "../../common/modelBase";
+import {TranslateService, TranslatePipe} from "ng2-translate/ng2-translate";
+import {Operacion} from "../operacion";
+import {OperacionSave} from "../methods";
+import moment from "moment/moment";
+
+
+@Component({
+    selector: 'operacion-pendiente',
+    templateUrl: 'app/operacion/pendiente/index.html',
+    styleUrls: ['app/operacion/pendiente/style.css'],
+    providers: [TranslateService,Operacion],
+    directives: [OperacionSave],
+    pipes: [TranslatePipe]
+})
+export class OperacionPendiente extends ModelBase implements OnInit {
+
+    public dataSelect:any = {};
+    constructor(public router:Router, public http:Http, public toastr:ToastsManager, public myglobal:globalService, public translate:TranslateService,public operacion:Operacion) {
+        super('OP', '/operations/', http, toastr, myglobal, translate);
+    }
+    ngOnInit(){
+        this.operacion.initModel();
+        
+        this.initModel();
+        this.setEndpoint('/pendings');
+        this.loadData();
+        this.setEndpoint('/operations');
+    }
+
+    initOptions() {
+        this.viewOptions["title"] = 'Operaciones pendientes';
+    }
+
+    initRules() {
+        this.rulesSave = this.operacion.rulesSave;
+    }
+
+    initSearch() {
+    }
+
+    initRuleObject() {
+    }
+
+    initFilter() {
+    }
+
+    initPermissions() {
+    }
+
+    goTaquilla(event, companyId:string) {
+        event.preventDefault();
+        let link = ['TaquillaSearh', {search: companyId}];
+        this.router.navigate(link);
+    }
+    @ViewChild(OperacionSave)
+    operacionSave:OperacionSave;
+
+    dataOperation:any={};
+    cargarData(event,data){
+        event.preventDefault();
+        this.dataOperation=data;
+        if(this.operacionSave) {
+            this.operacionSave.pendients=true;
+            this.operacionSave.inAntena(data);
+        }
+    }
+    formatDate(date, format) {
+        if (date)
+            return moment(date).format(format);
+        return "-";
+    }
+
+}
