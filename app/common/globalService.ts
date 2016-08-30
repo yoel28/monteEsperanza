915dfdc10b6d229035390edffe29481f1b62954c
yoel <yo3l.m18@gmail.com>
2016-08-30 16:49:30 -0400

    retorna verdadero.. solo para desarrollo

--- app/common/globalService.ts
+++ app/common/globalService.ts
@@ -50,7 +50,7 @@ export class globalService extends RestController{
         let index = this.permissions.findIndex(obj => obj.id == id);
         if(index > -1)
             return true;
-        return false;
+        return true;
     }
     myPermissions(){
         let that = this;
