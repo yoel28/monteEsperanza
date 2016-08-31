0e08df60d28956e6ce28f4bed472e640668e9556
yoel <yo3l.m18@gmail.com>
2016-08-31 17:05:04 -0400

    directiva de knob

--- app/common/xeditable.ts
+++ app/common/xeditable.ts
@@ -199,5 +199,17 @@ export class Datepicker implements OnInit {
     }
 }
 
+@Directive({
+    selector: "[knob]",
+    outputs:['elem']
+})
+export class Knob {
+    public elem:any;
 
+    constructor(el: ElementRef) {
+        let data = jQuery(el.nativeElement).knob();
+        this.elem = new EventEmitter();
+        this.elem.emit({data});
+    }
+}
 
