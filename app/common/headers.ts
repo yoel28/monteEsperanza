45b4646846f3ec5c2bb77aca72919963d79ddaad
yoel <yo3l.m18@gmail.com>
2016-09-16 17:07:15 -0400

    header timezone

--- app/common/headers.ts
+++ app/common/headers.ts
@@ -1,9 +1,10 @@
 import { Headers } from '@angular/http';
+declare var moment:any;
 
 export const contentHeaders = new Headers();
 contentHeaders.append('Accept', 'application/json');
 contentHeaders.append('Content-Type', 'application/json');
-//contentHeaders.append('Cache-Control', 'no-cache');
+contentHeaders.append('x-TimeZone',moment().format('Z'));
 
 if(localStorage.getItem('bearer'))
     contentHeaders.append('Authorization', 'Bearer '+localStorage.getItem('bearer'));
