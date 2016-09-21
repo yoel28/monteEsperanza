7042ef3c263d41040646073262f9b2a5d643454a
yoel <yo3l.m18@gmail.com>
2016-09-21 11:00:51 -0400

    dev

--- app/app.component.ts
+++ app/app.component.ts
@@ -49,7 +49,7 @@ import {OperacionPendiente} from "./operacion/pendiente/pendiente";
 
 declare var SockJS:any;
 declare var Stomp:any;
-let  version='/PROD';
+let  version='/DEV';
 
 @Component({
   selector: 'my-app',
