70e1848bba6ee6631df74152d3a2326851eef26b
yoel <yo3l.m18@gmail.com>
2016-09-23 10:56:20 -0400

    ruta principal appRoot

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,7 +5,7 @@
 (function(global) {
   // map tells the System loader where to look for things
   var map = {
-    'app':                        'app', // 'dist',
+    'app':                        'appRoot', // 'dist',
     '@angular':                   'node_modules/@angular',
     'rxjs':                       'node_modules/rxjs',
     'angular2-jwt':               'node_modules/angular2-jwt',
