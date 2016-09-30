b23fff33108e52af1aad36511bca3c741a195888
yoel <yo3l.m18@gmail.com>
2016-09-30 17:06:40 -0400

    router outlet nuevo

---
+++ appRoot/common/CustomRouterOutlet.ts
@@ -0,0 +1,30 @@
+import {Directive, Attribute, ViewContainerRef, DynamicComponentLoader} from '@angular/core';
+import {Router, RouterOutlet, ComponentInstruction} from '@angular/router-deprecated';
+
+@Directive({
+    selector: 'router-outlet-2'
+})
+
+export class CustomRouterOutlet extends RouterOutlet {
+    private parentRouter: Router;
+
+    constructor(_elementRef: ViewContainerRef, _loader: DynamicComponentLoader, _parentRouter: Router, @Attribute('name') nameAttr: string) {
+        super(_elementRef, _loader, _parentRouter, nameAttr);
+        this.parentRouter = _parentRouter;
+    }
+
+    activate(instruction: ComponentInstruction) {
+        console.log('activate');
+        return super.activate(instruction);
+    }
+
+    deactivate(instruction: ComponentInstruction) {
+        console.log('deactivate');
+        return super.deactivate(instruction);
+    }
+
+    reuse(instruction: ComponentInstruction) {
+        console.log('reuse');
+        return null;
+    }
+}
