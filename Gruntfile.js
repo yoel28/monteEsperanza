6df0c43f91bd70271643e3b523571b5de7c42f23
yo3l28 <yo3l.m18@gmail.com>
2016-10-05 19:50:19 -0400

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,25 @@ module.exports = function(grunt) {
         files: [{
             expand: true,    // ingresar a las subcarpetas
             cwd: './',  // ruta de nuestro javascript fuente          
-            src: ["**/*.js","!node_modules/**","!build/**","!assets/**",'!Gruntfile.js'],     // patrón relativo a cwd
+            src: ["**/*.js","!node_modules/**","!build/**",'!Gruntfile.js'],     // patrón relativo a cwd
             dest: 'build/'  // destino de los archivos compresos
             //ext: ".js"
         }]
       }              
     },
-    cssmin: {
+
+      cssmin: {
+          options: {
+              sourceMap:true,
+              roundingPrecision: -1
+          },
+          target: {
+              files: {
+                  'myapp.css': ["appRoot/**/*.css"]
+              }
+          }
+      },
+    cssmin_old: {
         target: {
             files: [{
             expand: true,
@@ -35,35 +47,25 @@ module.exports = function(grunt) {
         dist: {
             options: {
                 removeComments: true,
-                caseSensitive:true,
-                collapseWhitespace: true
+                caseSensitive: true,
+                collapseWhitespace: true,
+                collapseInlineTagWhitespace:true,
             },
             files: [{
                 expand: true,
                 cwd: './',
-                src: ["**/*.html","!node_modules/**","!build/**","!assets/**"],
+                src: ["**/*.html", "!node_modules/**", "!build/**"],
                 dest: 'build/'
             }]
         }
-    },
-      concat: {
-          options: {
-              separator: ';',
-              sourceMap:true,
-          },
-          dist: {
-              src: ['build/**/*.js',"!build/built.js"],
-              dest: 'build/built.js',
-          },
       },
   });
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks("grunt-ts");
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify','cssmin','htmlmin','concat']);
+  grunt.registerTask('default', ['uglify','cssmin','htmlmin']);
+  grunt.registerTask('css', ['cssmin']);
 
 };
