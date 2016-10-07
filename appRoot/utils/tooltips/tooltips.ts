ca5f8da68ece945c43716d9ef95711acca581668
yoel <yo3l.m18@gmail.com>
2016-10-07 17:31:58 -0400

    tooltip nuevo elemento

--- appRoot/utils/tooltips/tooltips.ts
+++ appRoot/utils/tooltips/tooltips.ts
@@ -1,21 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit,Inject,forwardRef} from '@angular/core';
 import {globalService} from "../../common/globalService";
+import {Help} from "../../help/help";
+import {Save} from "../save/save";
 declare var SystemJS:any;
+declare var moment:any;
+declare var jQuery:any;
 @Component({
     selector: 'tooltip',
     templateUrl: SystemJS.map.app+'/utils/tooltips/index.html',
     styleUrls: [SystemJS.map.app+'/utils/tooltips/style.css'],
+    
+    directives: [Save],
     inputs: ['code', 'params'],
 })
-export class Tooltips implements OnInit {
+export class Tooltip{
     public params:any={};
-    public code:any={};
+    public code="";
+    public data:any={};
+    public configId=moment().valueOf();
 
-    constructor(public myglobal:globalService) {
-
+    constructor(public prefix,public myglobal:globalService) {
+        //this.help.initModel();
     }
     ngOnInit() {
-
+        this.configId='TOOLTIP_'+this.configId+'_'+this.code;
+        if(this.code && this.code.length>0){
+            this.data=this.myglobal.getTooltip(this.code);
+        }
+    }
+    ngAfterViewInit(){
+        jQuery('#'+this.configId).popover();
     }
     f(event){
         event.preventDefault();
