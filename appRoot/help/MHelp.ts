c6314a17236a028a898917d4865ba6ed39f5dbd9
yoel <yo3l.m18@gmail.com>
2016-10-07 17:32:51 -0400

    elemneto nuevo help

---
+++ appRoot/help/MHelp.ts
@@ -0,0 +1,83 @@
+import {globalService} from "../common/globalService";
+import {ModelBase} from "../common/modelBase";
+
+export class MHelp extends ModelBase{
+    public rules={};
+    constructor(public myglobal:globalService){
+        super('INFO',myglobal);
+        this.initModel();
+    }
+    initRules(){
+        this.rules['code']={
+            'type': 'text',
+            'required':true,
+            'key': 'code',
+            'icon': 'fa fa-key',
+            'title': 'Codigo',
+            'placeholder': 'Ingrese el codigo',
+            'msg':{
+                'error':'Este campo es obligatorio',
+            }
+        }
+        this.rules['title']={
+            'type': 'text',
+            'required':true,
+            'key': 'title',
+            'icon': 'fa fa-key',
+            'title': 'Título',
+            'placeholder': 'Ingrese el título',
+            'msg':{
+                'error':'Este campo es obligatorio',
+            }
+        }
+        this.rules['color']={
+            'type': 'select',
+            'required':true,
+            'source': [
+                {'value': 'bg-transparent', 'text':'Transparente'},
+                {'value': 'bg-red', 'text':'Rojo'},
+                {'value': 'bg-blue', 'text': 'Azul'},
+                {'value': 'bg-yellow', 'text': 'Amarillo'},
+                {'value': 'bg-green', 'text': 'Verde'},
+                {'value': 'bg-black', 'text': 'Negro'},
+                {'value': 'bg-white', 'text': 'Blanco'},
+                {'value': 'bg-purple', 'text': 'Purpura'},
+                {'value': 'bg-fuchsia', 'text': 'Fucsia'},
+                {'value': 'bg-grey', 'text': 'Gris'},
+                {'value': 'bg-lime', 'text': 'Lima'},
+                {'value': 'bg-maroon', 'text': 'Marron'},
+                {'value': 'bg-olive', 'text': 'Oliva'},
+                {'value': 'bg-orange', 'text': 'Naranja'},
+                {'value': 'bg-pink', 'text': 'Rosado'},
+                {'value': 'bg-aqua', 'text': 'Azul claro'},
+            ],
+            'key': 'color',
+            'title': 'Color',
+            'placeholder': 'Seleccione el color',
+            'msg':{
+                'error':'Este campo es obligatorio',
+            }
+        }
+        this.rules['icon']={
+            'type': 'select',
+            'required':true,
+            'source': [
+                {'value': 'fa fa-question', 'text': 'question'},
+                {'value': 'fa fa-truck', 'text':'truck'},
+                {'value': 'fa fa-car', 'text': 'car'},
+                {'value': 'fa fa-bus', 'text': 'bus'},
+                {'value': 'fa fa-taxi', 'text': 'taxi'},
+                {'value': 'fa fa-list', 'text': 'list'},
+            ],
+            'key': 'icon',
+            'title': 'Icono',
+            'placeholder': 'Selecccione el icono',
+            'msg':{
+                'error':'Este campo es obligatorio',
+            }
+        }
+        this.rules = Object.assign({},this.rules,this.getRulesDefault())
+    }
+    initPermissions() {}
+
+}
---
+++ appRoot/help/help.ts
@@ -0,0 +1,99 @@
+import {Component, OnInit,ViewChild,Injectable} from '@angular/core';
+import {Router}           from '@angular/router-deprecated';
+import {Http} from '@angular/http';
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../common/globalService";
+import { ControllerBase} from "../common/ControllerBase";
+import {TranslateService, TranslatePipe} from "ng2-translate/ng2-translate";
+import {Filter} from "../utils/filter/filter";
+import {Tables} from "../utils/tables/tables";
+import {Save} from "../utils/save/save";
+import {Tooltip} from "../utils/tooltips/tooltips";
+import {MHelp} from "./MHelp";
+declare var SystemJS:any;
+
+@Component({
+    selector: 'help',
+    templateUrl: SystemJS.map.app+'/help/index.html',
+    styleUrls: [SystemJS.map.app+'/help/style.css'],
+    providers: [TranslateService],
+    directives: [Filter,Tables,Save],
+    pipes: [TranslatePipe]
+})
+@Injectable()
+export class Help extends ControllerBase implements OnInit {
+
+    public dataSelect:any = {};
+    public paramsTable:any={};
+    public model;
+
+    constructor(public router:Router, public http:Http, public toastr:ToastsManager, public myglobal:globalService, public translate:TranslateService) {
+        super('INFO', '/infos/', http, toastr, myglobal, translate);
+        this.model= new MHelp(myglobal);
+    }
+    ngOnInit(){
+        this.initModel();
+        this.loadParamsTable();
+        this.loadData();
+    }
+    initViewOptions() {
+        this.max=10;
+        
+        this.viewOptions["title"] = 'Informacion (Ayudas)';
+
+        this.viewOptions["buttons"].push({
+            'visible': this.permissions.add,
+            'title': 'Agregar',
+            'class': 'btn btn-green',
+            'icon': 'fa fa-save',
+            'modal': this.paramsSave.idModal
+        });
+
+        this.viewOptions["buttons"].push({
+            'visible': this.permissions.filter,
+            'title': 'Filtrar',
+            'class': 'btn btn-blue',
+            'icon': 'fa fa-filter',
+            'modal': this.paramsFilter.idModal
+        });
+        
+        this.viewOptions.actions.delete = {
+            'title': 'Eliminar',
+            'visible': this.permissions.delete,
+            'idModal': this.prefix+'_'+this.configId+'_del',
+            'message': 'Estás seguro que deseas eliminar la ayuda ',
+            'keyAction': 'code'
+        };
+    }
+    initRules() {
+        this.rules = this.model.rules;
+    }
+    initRulesSave(){
+        this.rulesSave=Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+    }
+    initParamsSearch() {}
+    initRuleObject() {}
+    initPermissions() {}
+    initRulesAudit() {}
+    initParamsSave() {}
+    initParamsFilter() {
+        this.paramsFilter.title="Filtrar ayudas";
+    }
+    loadParamsTable(){
+        this.paramsTable.endpoint=this.endpoint;
+        this.paramsTable.actions={};
+        this.paramsTable.actions.delete = {
+            "icon": "fa fa-trash",
+            "exp": "",
+            'title': 'Eliminar',
+            'idModal': this.prefix+'_'+this.configId+'_del',
+            'permission': this.permissions.delete,
+            'message': '¿ Esta seguro de eliminar la accion : ',
+            'keyAction':'code'
+        };
+    }
+    
+
+}
+
