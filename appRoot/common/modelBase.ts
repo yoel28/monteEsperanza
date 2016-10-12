1544225ee2c4bec6ee2eb5f3ff06b54275e77cc6
yoel <yo3l.m18@gmail.com>
2016-10-12 17:04:08 -0400

    nuevo modelbase

--- appRoot/common/modelBase.ts
+++ appRoot/common/modelBase.ts
@@ -1,39 +1,57 @@
 import {globalService} from "./globalService";
 
+declare var moment:any;
+
 export abstract class ModelBase{
 
     public prefix = "DEFAULT";
+    public endpoint = "DEFAULT_ENDPOINT";
     public permissions:any = {};
+    public paramsSearch:any = {};
+    public ruleObject:any={};
+    public ruleSave:any={};
+
+    public configId = moment().valueOf();
     private rulesDefault:any = {};
 
-    constructor(prefix,public myglobal:globalService){
+    constructor(prefix,endpoint,public myglobal:globalService){
         this.prefix = prefix;
+        this.endpoint = endpoint;
+
         this._initModel();
     }
 
     private _initModel(){
-        this._initPermissions(this.prefix);
+        this._initPermissions();
         this._initRules();
+        this._initParamsSearch();
+        this._initRuleObject();
     }
     public initModel(){
         this.initPermissions();
         this.initRules();
+        this.initRulesSave();
+        this.initParamsSearch();
+        this.initRuleObject();
     }
     
     
     abstract initPermissions();
-    private _initPermissions(prefix) {
+    private _initPermissions() {
         this.permissions['list'] = this.myglobal.existsPermission(this.prefix + '_LIST');
         this.permissions['add'] = this.myglobal.existsPermission(this.prefix + '_ADD');
         this.permissions['update'] = this.myglobal.existsPermission(this.prefix + '_UPDATE');
         this.permissions['delete'] = this.myglobal.existsPermission(this.prefix + '_DELETE');
         this.permissions['filter'] = this.myglobal.existsPermission(this.prefix + '_FILTER');
-        this.permissions['lock'] = this.myglobal.existsPermission(this.prefix + '_LOCK');
+        this.permissions['lock'] = this.myglobal.existsPermission(this.prefix + '_SEARCH');
+        this.permissions['search'] = this.myglobal.existsPermission(this.prefix + '_LOCK');
+        this.permissions['warning'] = this.myglobal.existsPermission(this.prefix + '_WARNING');
         this.permissions['visible'] = true;//this.myglobal.existsPermission(this.prefix + '_VISIBLE');
         this.permissions['audit'] = this.myglobal.existsPermission(this.prefix + '_AUDICT');
     }
 
     abstract initRules();
+    abstract initRulesSave();
     private _initRules() {
         this.rulesDefault["detail"] = {
             "update": this.permissions.update,
@@ -52,7 +70,7 @@ export abstract class ModelBase{
         this.rulesDefault["enabled"] = {
             "update": (this.permissions.update && this.permissions.lock),
             "visible": this.permissions.lock && this.permissions.visible,
-            "search": this.permissions.filter,
+            "search": false,
             'required': true,
             'icon': 'fa fa-list',
             "type": "boolean",
@@ -68,6 +86,45 @@ export abstract class ModelBase{
         };
     }
 
+    abstract initParamsSearch();
+    private _initParamsSearch() {
+        this.paramsSearch = {
+            'title': 'Title Default',
+            'permission': (this.permissions.search && this.permissions.list),
+            'idModal': this.prefix + '_' + this.configId + '_search',
+            'endpoint': "/search" + this.endpoint,
+            'placeholder': "Placeholder default",
+            'label': {'title': "titulo: ", 'detail': "detalle: "},
+            'msg': {
+                'errors': {
+                    'noAuthorized': 'No posee permisos para esta accion',
+                },
+            },
+            'where': '',
+            'imageGuest': '/assets/img/truck-guest.png'
+        };
+    }
+
+    abstract initRuleObject();
+    private _initRuleObject() {
+        this.ruleObject = {
+            'icon': 'fa fa-list',
+            "type": "text",
+            "required":false,
+            "key": "keyDefault",
+            "title": "TipoDefault",
+            'object': true,
+            "placeholder": "PlaceHolder default",
+            'paramsSearch': this.paramsSearch,
+            "permissions": this.permissions,
+            'msg': {
+                'errors': {
+                    'object': 'la referencia no esta registrada',
+                    'required': 'El campo es obligatorio'
+                },
+            }
+        }
+    }
 
     getRulesDefault(){
         return this.rulesDefault;
