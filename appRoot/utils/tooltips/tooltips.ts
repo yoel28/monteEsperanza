c0d84d9370850975021a1e5c9cbec8623c72204c
yoel <yo3l.m18@gmail.com>
2016-10-17 15:24:36 -0400

    normal

--- appRoot/utils/tooltips/tooltips.ts
+++ appRoot/utils/tooltips/tooltips.ts
@@ -1,19 +1,18 @@
-import {Component, OnInit,Inject,forwardRef} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {globalService} from "../../common/globalService";
-import {Help} from "../../help/help";
-import {Save} from "../save/save";
+
 declare var SystemJS:any;
 declare var moment:any;
 declare var jQuery:any;
+
 @Component({
     selector: 'tooltip',
     templateUrl: SystemJS.map.app+'/utils/tooltips/index.html',
     styleUrls: [SystemJS.map.app+'/utils/tooltips/style.css'],
-    
-    directives: [Save],
     inputs: ['code', 'params'],
 })
-export class Tooltip{
+export class Tooltip implements OnInit{
+
     public params:any={};
     public code="";
     public data:any={};
