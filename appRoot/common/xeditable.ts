bcf5791b2c0020cc501cf11d23e7d0e47473e849
yoel <yo3l.m18@gmail.com>
2016-10-18 17:33:29 -0400

    color (salida al ocultar elemnto)

--- appRoot/common/xeditable.ts
+++ appRoot/common/xeditable.ts
@@ -257,31 +257,38 @@ export class DrapResize {
 @Directive({
     selector: "[color-picker]",
     inputs:['hex'],
+    outputs:['color']
 })
 export class ColorPicker {
     public hide:any;
-    public hex:Control =  new Control('');
+    public hex:Control;
+    public color:any;
 
-    constructor(el:ElementRef) {
+    constructor(public element:ElementRef) {
+        this.hex = new Control('');
+        this.color = new EventEmitter();
+    }
+    ngOnInit(){
         let that = this;
-        
-        jQuery(el.nativeElement).ColorPicker({
+
+        jQuery(this.element.nativeElement).ColorPicker({
             color: that.hex.value,
             onShow: function (colpkr) {
                 jQuery(colpkr).fadeIn(500);
                 return false;
             },
             onHide: function (colpkr) {
+                that.color.emit(that.hex.value);
                 jQuery(colpkr).fadeOut(500);
                 return false;
             },
             onChange: function (hsb, hex, rgb) {
                 that.hex.updateValue(hex);
-                jQuery(el.nativeElement).css('backgroundColor', '#' + that.hex.value);
-                jQuery(el.nativeElement).val('#'+that.hex.value);
+                jQuery(that.element.nativeElement).css('backgroundColor', '#' + that.hex.value);
+                jQuery(that.element.nativeElement).val('#'+that.hex.value);
             }
         })
-        jQuery(el.nativeElement).css('backgroundColor', '#' + that.hex.value);
-        jQuery(el.nativeElement).val('#'+that.hex.value);
+        jQuery(that.element.nativeElement).css('backgroundColor', '#' + that.hex.value);
+        jQuery(that.element.nativeElement).val('#'+that.hex.value);
     }
 }
