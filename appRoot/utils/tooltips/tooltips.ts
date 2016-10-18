43e85767e96e6231aeb15f933967713304400485
yoel <yo3l.m18@gmail.com>
2016-10-18 11:32:28 -0400

    tooltip evento click para modificar el elemnto

--- appRoot/utils/tooltips/tooltips.ts
+++ appRoot/utils/tooltips/tooltips.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {globalService} from "../../common/globalService";
+import {MHelp} from "../../help/MHelp";
 
 declare var SystemJS:any;
 declare var moment:any;
@@ -9,17 +10,21 @@ declare var jQuery:any;
     selector: 'tooltip',
     templateUrl: SystemJS.map.app+'/utils/tooltips/index.html',
     styleUrls: [SystemJS.map.app+'/utils/tooltips/style.css'],
-    inputs: ['code', 'params'],
+    inputs: ['code'],
 })
 export class Tooltip implements OnInit{
 
-    public params:any={};
+    public permissions:any;
     public code="";
     public data:any={};
+    public mHelp:any;
+    
+
     public configId=moment().valueOf();
 
-    constructor(public prefix,public myglobal:globalService) {
-        //this.help.initModel();
+    constructor(public myglobal:globalService) {
+        this.mHelp=new MHelp(myglobal);
+        this.permissions = Object.assign({},this.mHelp.permissions);
     }
     ngOnInit() {
         this.configId='TOOLTIP_'+this.configId+'_'+this.code;
@@ -27,10 +32,22 @@ export class Tooltip implements OnInit{
             this.data=this.myglobal.getTooltip(this.code);
         }
     }
-    ngAfterViewInit(){
-        jQuery('#'+this.configId).popover();
+    ngAfterViewInit()
+    {
+        let that=this;
+        if(this.data && this.data.id){
+            jQuery('#'+this.configId).popover({
+                trigger: "hover"
+            });
+        }
     }
-    f(event){
+    edit(event,data){
         event.preventDefault();
+        if(this.permissions.update){
+            if(this.myglobal.objectInstance[this.mHelp.prefix]){
+                this.myglobal.objectInstance[this.mHelp.prefix].setLoadDataModel(data);
+            }
+        }
     }
+
 }
