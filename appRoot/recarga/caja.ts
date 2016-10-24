498219837220c0e52e125210e6687a821238e0b3
yoel <yo3l.m18@gmail.com>
2016-10-24 17:30:48 -0400

    caja nueva

--- appRoot/recarga/caja.ts
+++ appRoot/recarga/caja.ts
@@ -5,50 +5,113 @@ import {RestController} from "../common/restController";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
 import moment from 'moment/moment';
 import {globalService} from "../common/globalService";
-import {Datepicker} from "../common/xeditable";
+import {SMDropdown, DateRangepPicker} from "../common/xeditable";
+import {FormBuilder, ControlGroup, Control, Validators} from "@angular/common";
+
 declare var jQuery:any;
 declare var SystemJS:any;
+declare var Table2Excel:any;
 
 @Component({
     selector: 'caja',
     templateUrl: SystemJS.map.app+'/recarga/caja.html',
     styleUrls: [SystemJS.map.app+'/recarga/style.css'],
-    directives:[Datepicker]
+    directives:[DateRangepPicker,SMDropdown]
 })
 export class Caja extends RestController implements OnInit{
 
     public MONEY_METRIC_SHORT=this.myglobal.getParams('MONEY_METRIC_SHORT');
     public MONEY_METRIC=this.myglobal.getParams('MONEY_METRIC');
     public WEIGTH_METRIC_SHORT=this.myglobal.getParams('WEIGTH_METRIC_SHORT');
+    public paramsDate={'format':"DD-MM-YYYY","minDate":"01-01-2016"}
+
+
     public rechargeTotal={};
     public rechargeSum:any=[];
     public typeRecharge:any={};
     public dia:any;
 
-    constructor(public router: Router,public http: Http,public toastr: ToastsManager,public myglobal:globalService) {
+    form: ControlGroup;
+    dateStart:Control;
+    dateEnd:Control;
+
+    constructor(public router: Router,public http: Http,public _formBuilder: FormBuilder,public toastr: ToastsManager,public myglobal:globalService) {
         super(http,toastr);
         this.setEndpoint('/search/recharges/');
     }
     ngOnInit(){
         if(this.myglobal.existsPermission('109')){
-            this.max = 1000;
             this.dia = moment();
-            this.loadCaja();
+            this.initForm();
         }
     }
+    initForm(){
+        this.dateStart = new Control("", Validators.compose([Validators.required]));
+        this.dateEnd = new Control("");
+        this.form = this._formBuilder.group({
+            'dateStart': this.dateStart,
+            'dateEnd': this.dateEnd,
+        });
+    }
 
+    assignDate(data){
+        this.dateStart.updateValue(data.start || null);
+        this.dateEnd.updateValue(data.end || null);
+        this.loadCaja();
+    }
+
+    public itemsFecha=[
+        {'id':'1','text':'Hoy'},
+        {'id':'2','text':'Semana actual'},
+        {'id':'3','text':'Mes actual'},
+        {'id':'4','text':'Mes anterior'},
+        {'id':'5','text':'Últimos 3 meses'},
+        {'id':'6','text':'Año actual'},
+    ]
+    setFecha(id){
+        //Thu Jul 09 2015 00:00:00 GMT-0400 (VET)
+        let day = moment().format('lll');
+        let val;
+        switch (id)
+        {
+            case "1" : //hoy
+                this.dateStart.updateValue(moment().format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().add(1,'day').format('DD-MM-YYYY'));
+                break;
+            case "2" ://Semana Actual
+                this.dateStart.updateValue(moment().startOf('week').format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().endOf('week').format('DD-MM-YYYY'));
+                break;
+            case "3" ://mes actual
+                this.dateStart.updateValue(moment().startOf('month').format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().endOf('month').format('DD-MM-YYYY'));
+                break;
+            case "4" ://mes anterior
+                this.dateStart.updateValue(moment().subtract(1, 'month').startOf('month').format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().subtract(1, 'month').endOf('month').format('DD-MM-YYYY'));
+                break;
+            case "5" ://ultimos 3 meses
+                this.dateStart.updateValue(moment().subtract(3, 'month').startOf('month').format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().endOf('month').format('DD-MM-YYYY'));
+                break;
+            case "6" ://ano actual
+                this.dateStart.updateValue(moment().startOf('year').format('DD-MM-YYYY'));
+                this.dateEnd.updateValue(moment().endOf('year').format('DD-MM-YYYY'));
+                break;
+        }
+        if(id!='-1')
+            this.loadCaja();
+    }
     loadCaja(){
         this.max=20;
-        let hoy= this.dia.format('DD-MM-YYYY');
-        let manana= this.dia.add(1, 'days').format('DD-MM-YYYY')
-
-        this.where="&where="+encodeURI("[['op':'ge','field':'dateCreated','value':'"+hoy+"','type':'date'],['op':'lt','field':'dateCreated','value':'"+manana+"','type':'date']]");
+        this.where="&where="+encodeURI("[['op':'ge','field':'dateCreated','value':'"+this.dateStart.value+"','type':'date'],['op':'lt','field':'dateCreated','value':'"+this.dateEnd.value+"','type':'date']]");
         this.loadData();
         this.onloadData('/total/recharges/',this.rechargeTotal)
-        this.httputils.onLoadList('/type/recharges?max=1000',this.typeRecharge,this.max,this.error);
-        let where="&where="+encodeURI("[['op':'ge','field':'re.dateCreated','value':'"+hoy+"','type':'date'],['op':'lt','field':'re.dateCreated','value':'"+manana+"','type':'date']]");
+        let where="&where="+encodeURI("[['op':'ge','field':'re.dateCreated','value':'"+this.dateStart.value+"','type':'date'],['op':'lt','field':'re.dateCreated','value':'"+this.dateEnd.value+"','type':'date']]");
         this.onloadData('/operations/sum',this.rechargeSum,null,null,where)
     }
+    
+    
     getRechargeSum(val){
         let index = this.rechargeSum.findIndex(obj => (obj.companyTypeCredit == val ));
         if(index > -1)
@@ -56,18 +119,29 @@ export class Caja extends RestController implements OnInit{
         return 0;
 
     }
-    formatDate(date,format){
-        if(date)
-            return moment(date).format(format);
-        return "";
-    }
-    exportCSV(id,event?){
+    export(event?){
+        let that=this;
         if(event)
             event.preventDefault();
         this.getLoadDataAll([],null,null,0,1000,null,()=>{
-                setTimeout(function(_id=id,_jQuery=jQuery){ _jQuery("#"+_id).tableToCSV();}, 3000)
+                setTimeout(function(_jQuery=jQuery){
+                    that.exportCSV();
+                }, 3000)
             }
         )
+    }
+    exportCSV(){
+        let table2excel = new Table2Excel({
+            'defaultFileName': 'Caja',
+        });
+        Table2Excel.extend((cell, cellText) => {
+            if (cell) return {
+                v:cellText,
+                t: 's',
+            };
+            return null;
+        });
+        table2excel.export(document.querySelectorAll("table.export"));
     }
     goTaquilla(event, companyId:string) {
         event.preventDefault();
@@ -75,35 +149,11 @@ export class Caja extends RestController implements OnInit{
         this.router.navigate(link);
     }
     public typeRechargeIndex:any={};
-    calcRecharges(){
-        let that = this;
-        if(this.typeRecharge.list){
-            that.typeRechargeIndex={};
-            this.typeRecharge.list.forEach((obj,index)=>{
-                that.typeRechargeIndex[obj.id]=index;
-                obj['total']=0.0;
-            })
-            this.dataList.list.forEach(obj=>{
-                that.typeRecharge.list[that.typeRechargeIndex[obj.rechargeTypeId]].total+=obj.quantity;
-            })
-        }
-    }
-    
-    public formatDateFact = {
-        format: "dd-mm-yyyy",
-        startDate:'01-01-2016',
-        startView: 2,
-        minViewMode: 0,
-        maxViewMode: 2,
-        language: "es",
-        todayBtn: "linked",
-        forceParse: false,
-        autoclose: true,
-        todayHighlight: true,
+    formatDate(date,format){
+        if(date)
+            return moment(date).format(format);
+        return "";
     }
 
-    loadFechaFac(data) {
-        this.dia=moment(data.date);
-        this.loadCaja();
-    }
+
 }
