2c4f550998a35c998519f8932dcc089d69802b45
yoel <yo3l.m18@gmail.com>
2016-10-24 11:44:07 -0400

    exportar a excel, tabla

--- appRoot/reportes/gruposRutas.ts
+++ appRoot/reportes/gruposRutas.ts
@@ -11,6 +11,7 @@ import {Datepicker} from "../common/xeditable";
 import {Fecha} from "../utils/pipe";
 import {Search} from "../utils/search/search";
 declare var SystemJS:any;
+declare var Table2Excel:any;
 
 @Component({
     selector: 'reporte-gruposRutas',
@@ -26,11 +27,14 @@ export class GruposRutas extends RestController implements OnInit{
     dateEnd:Control;
     fechaRegistro:any;
 
+    public title:string;
+
     constructor(public router: Router,public http: Http,toastr:ToastsManager,public myglobal:globalService,public _formBuilder: FormBuilder) {
         super(http,toastr);
         this.setEndpoint('/reports/routes');
     }
     ngOnInit(){
+        this.title="REPORTE POR RUTAS";
         this.initForm();
         this.getRoutes();
     }
@@ -135,6 +139,20 @@ export class GruposRutas extends RestController implements OnInit{
         popupWin.document.head.innerHTML = (document.head.innerHTML);
         popupWin.document.close();
     }
+    exportCSV(){
+        let table2excel = new Table2Excel({
+            'defaultFileName': this.title,
+        });
+        Table2Excel.extend((cell, cellText) => {
+            if (cell) return {
+                v:cellText,
+                t: 's',
+            };
+            return null;
+        });
+        table2excel.export(document.querySelectorAll("table.export"));
+    }
+    
     public routes:any={};
     public idRoute:string="-1";
     public routeName:string="-1";
