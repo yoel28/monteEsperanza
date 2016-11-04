9dca896c72a2037a4bea5268b9540a2d8d63fc0f
yoel <yo3l.m18@gmail.com>
2016-11-04 14:09:58 -0400

    nuevo ruta

--- appRoot/ruta/ruta.ts
+++ appRoot/ruta/ruta.ts
@@ -24,58 +24,38 @@ export class Ruta extends ControllerBase implements OnInit {
 
     public dataSelect:any = {};
     public paramsTable:any={};
-    public model;
 
     constructor(public router:Router, public http:Http, public toastr:ToastsManager, public myglobal:globalService, public translate:TranslateService) {
         super('ROUTE', '/routes/',router, http, toastr, myglobal, translate);
-        this.model= new MRuta(myglobal);
     }
     ngOnInit(){
         this.initModel();
+        this.initViewOptions();
         this.loadParamsTable();
         this.loadPage();
     }
-    initPermissions() {
-        this.permissions = this.model.permissions;
+    initModel() {
+        this.model= new MRuta(this.myglobal);
     }
     initViewOptions() {
-        this.max=10;
-
         this.viewOptions["title"] = 'Ruta';
-
+        this.viewOptions["buttons"] = [];
         this.viewOptions["buttons"].push({
-            'visible': this.permissions.add,
+            'visible': this.model.permissions.add,
             'title': 'Agregar',
             'class': 'btn btn-green',
             'icon': 'fa fa-save',
-            'modal': this.paramsSave.idModal
+            'modal': this.model.paramsSave.idModal
         });
 
         this.viewOptions["buttons"].push({
-            'visible': this.permissions.filter,
+            'visible': this.model.permissions.filter,
             'title': 'Filtrar',
             'class': 'btn btn-blue',
             'icon': 'fa fa-filter',
-            'modal': this.paramsFilter.idModal
+            'modal': this.model.paramsSearch.idModal
         });
 
-    }
-    initRules() {
-        this.rules = this.model.rules;
-    }
-    initRulesSave(){
-        this.rulesSave=this.model.rulesSave;
-    }
-    initParamsSearch() {
-        this.paramsSearch.placeholder="Buscar ruta";
-    }
-    initRuleObject() {}
-    initRulesAudit() {}
-    initParamsSave() {
-        this.paramsSave.title="Agregar ruta";
-    }
-    initParamsFilter() {
-        this.paramsFilter.title="Filtrar ruta";
     }
     loadParamsTable(){
         this.paramsTable.endpoint=this.endpoint;
@@ -85,7 +65,7 @@ export class Ruta extends ControllerBase implements OnInit {
             "exp": "",
             'title': 'Eliminar',
             'idModal': this.prefix+'_'+this.configId+'_del',
-            'permission': this.permissions.delete,
+            'permission': this.model.permissions.delete,
             'message': '¿ Esta seguro de eliminar la ruta : ',
             'keyAction':'reference'
         };
