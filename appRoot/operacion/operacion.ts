239f9198bccd5e154ab9e67a302cbfe910e8c24d
yoel <yo3l.m18@gmail.com>
2016-11-21 17:48:44 -0400

    comentario

--- appRoot/operacion/operacion.ts
+++ appRoot/operacion/operacion.ts
@@ -48,8 +48,8 @@ export class Operacion extends ControllerBase implements OnInit {
             let end = moment().endOf('month').add('1','day').format('DD-MM-YYYY');
 
             this.where="&where="+encodeURI("[['op':'ge','field':'dateCreated','value':'"+start+"','type':'date'],['op':'le','field':'dateCreated','value':'"+end+"','type':'date']]");
-            if (localStorage.getItem('view9'))
-                this.view = JSON.parse(localStorage.getItem('view9'));
+            if (localStorage.getItem('view10'))
+                this.view = JSON.parse(localStorage.getItem('view10'));
             this.ordenView();
             this.loadData();
         }
@@ -188,6 +188,7 @@ export class Operacion extends ControllerBase implements OnInit {
         {'visible': false, 'position': 13, 'title': 'Fecha de Entrada', 'key': 'dateCreated'},
         {'visible': true, 'position': 14, 'title': 'Chofer', 'key': 'choferName'},
         {'visible': true, 'position': 15, 'title': 'Contenedor', 'key': 'containerCode'},
+        {'visible': true, 'position': 16, 'title': 'Comentario', 'key': 'comment'},
 
     ];
 
@@ -234,7 +235,7 @@ export class Operacion extends ControllerBase implements OnInit {
                 }
             })
         }
-        localStorage.setItem('view9', JSON.stringify(this.view))
+        localStorage.setItem('view10', JSON.stringify(this.view))
     }
 
     setVisibleView(data) {
@@ -244,7 +245,7 @@ export class Operacion extends ControllerBase implements OnInit {
                 return;
             }
         })
-        localStorage.setItem('view9', JSON.stringify(this.view))
+        localStorage.setItem('view10', JSON.stringify(this.view))
     }
 
     edit(data){
