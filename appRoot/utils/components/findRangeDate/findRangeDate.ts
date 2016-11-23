fba69f274299b826b52e441b5fd1bafeb0d441cb
yoel <yo3l.m18@gmail.com>
2016-11-23 10:35:41 -0400

    componente  para buscar fechas por rango

---
+++ appRoot/utils/components/findRangeDate/findRangeDate.ts
@@ -0,0 +1,42 @@
+import {Component, EventEmitter, OnInit,AfterViewInit} from "@angular/core";
+import {Control} from "@angular/common";
+import {SMDropdown, DateRangepPicker} from "../../../common/xeditable";
+import {globalService} from "../../../common/globalService";
+import {CatalogApp} from "../../../common/catalogApp";
+declare var SystemJS:any;
+@Component({
+    selector: 'find-range-date',
+    templateUrl: SystemJS.map.app+'/utils/components/findRangeDate/index.html',
+    styleUrls: [SystemJS.map.app+'/utils/components/findRangeDate/style.css'],
+    directives:[SMDropdown,DateRangepPicker],
+    inputs:['params','control'],
+    outputs:['dateRange'],
+})
+export class FindRangeDate implements OnInit,AfterViewInit {
+    public control:Control;
+    public params:any;
+    public dateRange:any;
+
+    public paramsDate = CatalogApp.formatDateDDMMYYYY;
+    public itemsDate = CatalogApp.itemsDate;
+
+    constructor(public myglobal:globalService) {
+        this.control = new Control('');
+        this.dateRange = new EventEmitter();
+    }
+    ngOnInit(){
+
+    }
+    ngAfterViewInit(){
+
+    }
+    setFecha(id){
+        if(id!='-1'){
+            this.control.updateValue(CatalogApp.getDateRange(id));
+            this.dateRange.emit(this.control);
+        }
+    }
+    assignDate(value){
+        this.control.updateValue(value);
+    }
+}
