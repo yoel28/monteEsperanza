96dd75775dc7a586661c40e360c8ccd4f5e3a06b
yoel <yo3l.m18@gmail.com>
2017-02-21 16:56:40 -0400

    add place

---
+++ appRoot/place/MPlace.ts
@@ -0,0 +1,46 @@
+import {globalService} from "../common/globalService";
+import {ModelBase} from "../common/modelBase";
+
+export class MPlace extends ModelBase{
+    public rules={};
+    constructor(public myglobal:globalService){
+        super('PLACE','/places/',myglobal);
+        this.initModel();
+    }
+    modelExternal() {}
+    initRules(){
+
+        this.rules['title']={
+            'type': 'text',
+            'required':true,
+            'update':this.permissions.update,
+            'search':this.permissions.filter,
+            'visible':this.permissions.visible,
+            'key': 'title',
+            'title': 'Título',
+            'placeholder': 'Título',
+        };
+        this.rules = Object.assign({},this.rules,this.getRulesDefault());
+        this.rules['detail'].required  = true;
+    }
+    initPermissions() {}
+    initParamsSearch() {
+        this.paramsSearch.title="Buscar lugar";
+        this.paramsSearch.placeholder="Ingrese el lugar";
+    }
+    initParamsSave() {
+        this.paramsSave.title="Agregar lugar"
+    }
+    initRuleObject() {
+        this.ruleObject.title="Lugares";
+        this.ruleObject.placeholder="Ingrese lugar";
+        this.ruleObject.key="place";
+        this.ruleObject.keyDisplay="placeTitle";
+        this.ruleObject.code="placeId";
+    }
+    initRulesSave() {
+        this.rulesSave = Object.assign({},this.rules);
+        delete this.rulesSave.enabled;
+    }
+
+}
