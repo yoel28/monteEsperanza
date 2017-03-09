2a5a7b8cbce1b9de144ee3b6e09f08b40c998ae5
suarezy <suarezy@zippyttech.com>
2017-03-09 15:16:41 -0400

    nuevo: componente  -> IgaleriaData , componente de galeria de imagenes

---
+++ appRoot/operacion/galeria/galeriaFolder.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit} from '@angular/core';
+import {globalService} from "../../common/globalService";
+declare var SystemJS:any;
+
+
+export interface IGaleriaData{
+    title?:string;
+    id:string,
+    images:{ folder:string, created:string }[];
+    selectFolder?:string
+    selectImage?:string
+
+}
+@Component({
+    selector: 'modalGaleriac',
+    templateUrl: SystemJS.map.app+'/operacion/galeria/index.html',
+    styleUrls: [SystemJS.map.app+'/operacion/galeria/style.css'],
+    inputs:['data']
+})
+export class galeriaComponent implements OnInit {
+
+    public data:IGaleriaData;
+    public server:string;
+    constructor(public myglobal:globalService){
+        this.server = this.myglobal.getParams('SERVER_IMAGE');
+    }
+
+    public listImage=[1,2,3,4,5,6,7,8,9];
+
+    openFolder(folder){
+        this.data.selectFolder = folder;
+    }
+    ngOnInit(){
+
+    }
+
+}
