c00d8fbfdae65bec4265782ef6f45d38ae986213
yoel <yo3l.m18@gmail.com>
2017-05-12 15:16:48 -0400

    add zone

---
+++ appRoot/zone/zone.ts
@@ -0,0 +1,55 @@
+import {Component, OnInit,AfterViewInit} from '@angular/core';
+import {globalService} from "../common/globalService";
+import {BaseView} from "../utils/baseView/baseView";
+import {MZone} from "./MZone";
+declare var SystemJS:any;
+
+@Component({
+    selector: 'zone',
+    templateUrl:SystemJS.map.app+'/utils/baseView/base.html',
+    styleUrls: [SystemJS.map.app+'/utils/baseView/style.css'],
+    directives: [BaseView],
+})
+export class Zone implements OnInit,AfterViewInit{
+
+    public instance:any={};
+    public paramsTable:any={};
+    public model:any;
+    public viewOptions:any={};
+
+    constructor(public myglobal:globalService) {}
+
+    ngOnInit(){
+        this.initModel();
+        this.initViewOptions();
+        this.loadParamsTable();
+    }
+
+    ngAfterViewInit():any {
+        this.instance = {
+            'model':this.model,
+            'viewOptions':this.viewOptions,
+            'paramsTable':this.paramsTable
+        };
+    }
+
+    initModel() {
+        this.model= new MZone(this.myglobal);
+    }
+
+    initViewOptions() {
+        this.viewOptions["title"] = 'Zona';
+    }
+
+    loadParamsTable(){
+        this.paramsTable.actions={};
+        this.paramsTable.actions.delete = {
+            'message': '¿ Esta seguro de eliminar la zona  : ',
+            'keyAction':'code'
+        };
+    }
+
+
+}
+
+
