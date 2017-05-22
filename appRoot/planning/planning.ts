3f4ef35a617c44f2600505723f15eb5058bbf813
yo3l28 <yo3l.m18@gmail.com>
2017-05-22 09:09:20 -0400

    add buttons exports

--- appRoot/planning/planning.ts
+++ appRoot/planning/planning.ts
@@ -4,6 +4,8 @@ import {BaseView} from "../utils/baseView/baseView";
 import {MPlanning} from "./MPlanning";
 
 declare var SystemJS:any;
+declare var moment:any;
+
 @Component({
     selector: 'planning',
     templateUrl:SystemJS.map.app+'/utils/baseView/base.html',
@@ -39,6 +41,41 @@ export class Planning implements OnInit,AfterViewInit{
 
     initViewOptions() {
         this.viewOptions["title"] = 'Planificación';
+        this.viewOptions["buttons"]=[];
+
+        this.viewOptions["buttons"].push({
+            'visible': this.model.permissions.list,
+            'title': 'Exportar en formato PDF',
+            'class': 'btn text-red btn-box-tool',
+            'icon': 'fa fa-file-pdf-o',
+            'callback':(event:Event,bv:BaseView)=>{
+                event.preventDefault();
+                let url = localStorage.getItem('urlAPI') +
+                    this.model.endpoint +
+                    '?access_token=' + localStorage.getItem('bearer') +
+                    bv.where+
+                    '&formatType=pdf' +
+                    '&tz=' + moment().format('Z').replace(':', '');
+                window.open(url, '_blank');
+            }
+        });
+
+        this.viewOptions["buttons"].push({
+            'visible': this.model.permissions.list,
+            'title': 'Exportar en formato XLS',
+            'class': 'btn text-green btn-box-tool',
+            'icon': 'fa fa-file-excel-o',
+            'callback':(event:Event,bv:BaseView)=>{
+                event.preventDefault();
+                let url = localStorage.getItem('urlAPI') +
+                    this.model.endpoint +
+                    '?access_token=' + localStorage.getItem('bearer') +
+                    bv.where+
+                    '&formatType=xls' +
+                    '&tz=' + moment().format('Z').replace(':', '');
+                window.open(url, '_blank');
+            }
+        });
     }
 
     loadParamsTable(){
