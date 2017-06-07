732cdabfea45a759f4884cfdf926ae13b7b95965
yoel <yo3l.m18@gmail.com>
2017-06-07 09:29:11 -0400

    date time picker

--- appRoot/com.zippyttech.ui/directive/date-time-picker/date-time-picker.ts
+++ appRoot/com.zippyttech.ui/directive/date-time-picker/date-time-picker.ts
@@ -1,9 +1,5 @@
-import {Component, EventEmitter, OnInit, AfterViewInit, Directive, ElementRef} from "@angular/core";
-import {Control} from "@angular/common";
-import {globalService} from "../../common/globalService";
-import {FindRangeDate} from "../components/findRangeDate/findRangeDate";
+import {EventEmitter, OnInit, Directive, ElementRef} from "@angular/core";
 
-declare var SystemJS:any;
 declare var moment:any;
 declare var jQuery:any;
 
@@ -24,7 +20,7 @@ export class DateTimePicker implements OnInit {
         let that = this;
         that.element = jQuery(this.el.nativeElement).datetimepicker();
         jQuery(this.el.nativeElement).datetimepicker().on('dp.change',  (ev) => {
-            that.fecha.emit(moment(ev.date).format('YYYY-MM-DD HH:mm:ssZ'));
+            that.fecha.emit(moment(ev.date).format('YYYY-MM-DD HH:mm:ssX'));
             console.log(moment(ev.date).format('YYYY-MM-DD HH:mmZ'))
         });
     }
